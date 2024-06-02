52c14d4e4f35d56ec4801db605dcc0c9b2db14e7
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2024-06-02 17:01:48 -0300

    Alimentando o banco estaticamente

--- Entity_Sql_Server/Program.cs
+++ Entity_Sql_Server/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Entity_Sql_Server
+﻿using Entity_Sql_Server.Entidades;
+
+namespace Entity_Sql_Server
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var context = new AppDbContext())
+            {
+                var usuario = new Usuario()
+                {
+                    NomeUser = "Pedro Siqueira Pereira Bitarães",
+                    EmailUser = "pedrosiqueirapb@gmail.com",
+                    SenhaUser = "12345",
+                    EnderecoUser = "Rua Central, 123\r\nBairro São José\r\nCidade Exemplo",
+                    LembreteSenha = "Sequência de números",
+                    IdadeUser = 20,
+                    SexoUser = 'M'
+                };
+                context.Usuarios.Add(usuario);
+                context.SaveChanges();
+
+                var produto = new Produto()
+                {
+                    NomeProd = "Camisa Cruzeiro temporada 2024",
+                    PrecoProd = 250.0,
+                    DescricaoProd = "Camisa oficial do Cruzeiro utilizada pelos jogadores na temporada 2024",
+                    QuantidadeProd = 100,
+                    AvaliacaoProd = 5,
+                    CategoriaProd = "Esporte",
+                    UserId = usuario.Id
+                };
+                context.Produtos.Add(produto);
+                context.SaveChanges();
+            }
         }
     }
 }
